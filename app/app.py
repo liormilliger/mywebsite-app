83727ac86469e07b42023065eb704abb6cfc4cf1
Lior Milliger <liormilliger@gmail.com>
2025-09-16 23:47:52 +0300

    updated home page layout and content and added routes and pages

--- app/app.py
+++ app/app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
 
-# Initialize the Flask application
 app = Flask(__name__)
 
-# Define the route for the homepage
+# Route for the homepage
 @app.route('/')
 def home():
-    """Renders the homepage."""
     return render_template('index.html')
 
-# This allows you to run the app directly
+# Route for "Tech in Action" page
+@app.route('/tech-in-action')
+def tech():
+    return render_template('tech.html')
+
+# Route for "My Projects" page
+@app.route('/my-projects')
+def projects():
+    return render_template('projects.html')
+
+# Route for "Education" page
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
