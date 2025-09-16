5e0f058700bd747f0b2244e5d5dca1ce709093b3
Lior Milliger <liormilliger@gmail.com>
2025-09-16 22:39:08 +0300

    new file structure and page layout with photo cv and links

--- app/app.py
+++ app/app.py
@@ -1,18 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Define the route for the home page
+# Define the route for the homepage
 @app.route('/')
 def home():
-    """
-    Serves the index.html file from the current directory.
-    """
-    return send_from_directory('.', 'index.html')
+    """Renders the homepage."""
+    return render_template('index.html')
 
-# Main entry point to run the application
+# This allows you to run the app directly
 if __name__ == '__main__':
-    # Runs the Flask app on http://0.0.0.0:5000/
-    # The debug=True flag provides detailed error pages and auto-reloads the server on code changes.
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
