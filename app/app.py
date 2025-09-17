f59c92b12dcf5730cb3154b6108c5b20d718ab95
Lior Milliger <liormilliger@gmail.com>
2025-09-17 08:21:14 +0300

    Saperating about me and home pages

--- app/app.py
+++ app/app.py
@@ -2,22 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Route for the homepage
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Route for "Tech in Action" page
+@app.route('/about-me')
+def about():
+    return render_template('about.html')
+
 @app.route('/tech-in-action')
 def tech():
     return render_template('tech.html')
 
-# Route for "My Projects" page
 @app.route('/my-projects')
 def projects():
     return render_template('projects.html')
 
-# Route for "Education" page
 @app.route('/education')
 def education():
     return render_template('education.html')
