2a2ddce1736db04f0c154fe10e409824c5521025
Lior Milliger <liormilliger@gmail.com>
2025-10-03 14:05:42 +0300

    init.sql for db

--- init.sql
+++
@@ -1,9 +0,0 @@
--- This script will be executed when the container is first started.
-
-CREATE TABLE IF NOT EXISTS traffic_log (
-    log_id SERIAL PRIMARY KEY,
-    visitor_ip VARCHAR(45) NOT NULL,
-    page_visited TEXT NOT NULL,
-    access_timestamp TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-
